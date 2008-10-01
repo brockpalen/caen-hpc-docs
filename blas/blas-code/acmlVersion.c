0a1c36d6cb1670667d5d062400905d793ec8d9df
Brock E. Palen cac <brockp@umich.edu>
2008-10-01 11:04:33 -0400

    added acmlVersion.c
    Added c souce to find which version of acml is in use

---
+++ blas/blas-code/acmlVersion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <acml.h>
+
+int main(void){
+  int *major, *minor, *patch;
+  /*acmlversion(major,minor,patch);*/
+  acmlinfo();
+  /*printf("acml version: %d.%d.%d\n",major, minor, patch);*/
+
+return 0;
+}
