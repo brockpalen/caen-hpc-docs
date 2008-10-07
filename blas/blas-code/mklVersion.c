83b3ac817e38e668da341eacdf4043301e805224
Brock E. Palen cac <brockp@umich.edu>
2008-10-07 10:29:53 -0400

    added OpenMP checks to the BLAS library checks

--- blas/blas-code/mklVersion.c
+++ blas/blas-code/mklVersion.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mkl.h>
+#ifdef _OPENMP
+  #include <omp.h>
+#endif
 
 int main(void){
   MKLVersion pVersion;
@@ -12,6 +15,12 @@ int main(void){
 
   printf("Build: %s\n", pVersion.Build);
   printf("Detected CPU %s\n", pVersion.Processor);
+
+  #ifdef _OPENMP
+   printf("\nOpenMP Enabled OMP_NUM_THREADS=%d CPU's of %d CPU's Total\n\n",
+           omp_get_max_threads(),
+           omp_get_num_procs());
+  #endif  /* OpenMP check */
 
 return 0;
 }
